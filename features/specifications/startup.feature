178dfa7aebaf57467f841e2ae1d7a0d54dfa3368
Joslaine <56135638+jopospor@users.noreply.github.com>
2022-03-30 13:51:12 -0300

    Teste

--- features/specifications/startup.feature
+++ features/specifications/startup.feature
@@ -6,21 +6,18 @@ A fim de manipular e cadastrar informações
 
 Contexto:
     Dado que o usuário possui chave e token da API do Trello 
+    E já existe um quadro criado com identificador disponível
+    E este quadro possui o Identificador de uma das listas presentes no quadro
 
-Cenário: Criar Cartão
-  E já existe um quadro criado com identificador disponível
-  E este quadro possui o Identificador de uma das listas presentes no quadro
+Cenário: Validações API do Trello
   Quando realizar uma requisição para criar cartão
   Então o cartão deve ser criado
 
-Cenário: Editar Cartão
   Quando realizar uma requisição para editar cartão 
   Então a edição do cartão será efetuada  
 
-Cenário: Excluir Cartão
   Quando realizar uma requisição para excluir cartão 
   Então o cartão será excluído
 
-Cenário: Criar cartão sem passar o identificador de uma das lista
   Quando realizar uma requisição para criar cartão sem passar o identificador de uma das lista
   Então espera mensagem de erro
--- features/step_definitions/startup_steps.rb
+++ features/step_definitions/startup_steps.rb
@@ -6,11 +6,11 @@ Dado('que o usuário possui chave e token da API do Trello') do
     @base_url = "https://api.trello.com/1/cards"
   end
   
-  Dado('já existe um quadro criado com identificador disponível') do
+  E('já existe um quadro criado com identificador disponível') do
     @board_id = '6242e6b50d14be1fb2deae0a'
   end
   
-  Dado('este quadro possui o Identificador de uma das listas presentes no quadro') do
+  E('este quadro possui o Identificador de uma das listas presentes no quadro') do
     @list_id = '6242e6b50d14be1fb2deae0b'
   end
   
@@ -32,12 +32,12 @@ Dado('que o usuário possui chave e token da API do Trello') do
   
   Quando('realizar uma requisição para editar cartão') do
     @updating_params = {
-        name: 'new name',
-        desc: 'new desc',
-        idList: @list_id
-      }
-    
-      @response = HTTParty.put("#{@base_url}/#{@card_id}?#{@auth}", { body: @updating_params })
+      name: 'new name',
+      desc: 'new desc',
+      idList: @list_id
+    }
+  
+    @response = HTTParty.put("#{@base_url}/#{@card_id}?#{@auth}", { body: @updating_params })
   end
   
   Então('a edição do cartão será efetuada') do
@@ -56,7 +56,7 @@ Dado('que o usuário possui chave e token da API do Trello') do
   end
   
   Então('o cartão será excluído') do
-    expect(@response.code).to eq(404)
+    expect(@response.code).to eq(200)
   end
   
   Quando('realizar uma requisição para criar cartão sem passar o identificador de uma das lista') do
